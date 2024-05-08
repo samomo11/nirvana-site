ff5faf5a3790cf786da437f322a81d9d1ff6fe71
samomo11 <samomo@dons.usfca.edu>
2024-05-07 17:52:17 -0700

    minor changes

--- js/lights-out.js
+++ js/lights-out.js
@@ -13,10 +13,10 @@ function setup() {
 }
 
 function draw() {
-  mappedYposition = map(mouseY, 0, height, 0, 255);
+  mappedYposition = map(mouseY, 0, height, 0, 300);
   background(mappedYposition);
   fill(255, 255, 255);
-  text("With the lights out", width / 2, height / 10);
+  text("With the lights out", width / 2, height / 7);
   fill(0, 0, 0);
-  text("it's less dangerous", width / 2, height - 50);
+  text("it's less dangerous", width / 2, height - 80);
 }
